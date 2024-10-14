7706fde60b978da0bbcfae2339349662302ba8d1
valesj02 <valesj02@jcu.cz>
2024-10-14 15:50:20 +0000

    mensi upravy

--- second.py
+++ second.py
@@ -2,20 +2,20 @@ def cislo_text(cislo):
     # funkce zkonvertuje cislo do jeho textove reprezentace
     # napr: "25" -> "dvacet pět", omezte se na cisla od 0 do 100
     small_numbers = ["nula", "jedna", "dva", "tři", "čtyři", "pět", "šest", "sedm", "osm", "devět"]
-    teens = ["deset", "jedenáct", "dvanáct", "třináct", "čtrnáct", "patnáct", "šestnáct", "sedmnáct", "osmnáct", "devatenáct"]
+    larger_numbers = ["deset", "jedenáct", "dvanáct", "třináct", "čtrnáct", "patnáct", "šestnáct", "sedmnáct", "osmnáct", "devatenáct"]
     desitky = ["","", "dvacet", "třicet", "čtyřicet", "padesát", "šedesát", "sedmdesát", "osmdesát", "devadesát"]
     
     if cislo < 10:
         return small_numbers[cislo]
     elif cislo < 20:
-        return teens[cislo - 10]
+        return larger_numbers[cislo - 10]
     elif cislo < 100:
-        desitka = cislo // 10 #zaokrouhlí výsledek dolů
-        jednotka = cislo % 10 #zbytek
-        if jednotka == 0:
-            return desitky[desitka]
+        zaokrouhleni = cislo // 10 #zaokrouhlí výsledek dolů
+        zbytek = cislo % 10 #zbytek
+        if zbytek == 0:
+            return desitky[zaokrouhleni]
         else:
-            return desitky[desitka] + " " + small_numbers[jednotka]
+            return desitky[zaokrouhleni] + " " + small_numbers[zbytek]
     elif cislo == 100:
         return "sto"
     else:
